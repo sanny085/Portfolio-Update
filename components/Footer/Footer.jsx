663850c3f1873fea0839f133cd9cd09aa195b611
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-29 15:11:55 +0530

    Update Footer.jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
+import Typography from '@material-ui/core/Typography';
+import ResumeData from '../../utils/resumeDate';
+
+import './Footer.css';
+
 function Footer() {
-    return (
-        <div>
+ return (
+        <div className="footer">
+           <div className="footer_left">
+             <Typography className="footer_name"  >
+                    {ResumeData.name}
+             </Typography>
+           </div>
            
+           <div className="footer_right">
+            <Typography className="footer_copyright">
+               Designed And Developed By &#9825;{" "}
+               <a href="./" >
+               {ResumeData.name}
+               </a>
+               <br/>
+               &#169;{" "+(new Date().getFullYear())} Clone Idea From {" "}
+               <a href="https://themeforest.net/item/mat-html-vcard-template/21062060" target="_blank">
+                   Tavonline
+               </a>.
+             </Typography>
+           </div>
+
         </div>
     )
 }
