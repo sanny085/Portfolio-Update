c6c31855bd8a23ac89ba7308a7a522587d2a9a54
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-31 15:06:45 +0530

    Update Header.jsx

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -16,18 +16,17 @@ function Header(props) {
     const pathname = props?.location?.pathname;
     return (
     <>
-    <Navbar expand="lg" sticky="top" className="header">
-       
+    <Navbar collapseOnSelect expand="lg" sticky="top" className="header">
         <Nav.Link as={NavLink} to="./" className="header_navlink"> 
           <Navbar.Brand className="header_home">
              <HomeRounded />
           </Navbar.Brand>
         </Nav.Link>
 
-        <Navbar.Toggle />
-
-        <Navbar.Collapse>
-          <Nav className="mr-auto">
+      <Navbar.Toggle />
+      
+      <Navbar.Collapse>
+         <Nav className="mr-auto">
             {/*Resume link*/}
             <Nav.Link as={NavLink} to="./" className={pathname == "/" ? "header_link_active" : "header_link"}>
              Resume
@@ -48,9 +47,41 @@ function Header(props) {
             <CustomButton text={"Hire Me"} icon={<Telegram />}  />
            </a>   
           </div>
+         
+        
         </Navbar.Collapse>
-
      </Navbar>
+
+<>
+  <Navbar collapseOnSelect expand="lg"   className="header">
+  <Navbar.Brand href="#home" className="header_home">
+      <HomeRounded />
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+</>
+
+
+
    </>
     );
 }
