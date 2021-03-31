d65622417c4dacddfd5ff071320b7bde72a0c82e
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-03-31 15:47:16 +0530

    Update Header.jsx

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -16,73 +16,43 @@ function Header(props) {
     const pathname = props?.location?.pathname;
     return (
     <>
-    <Navbar collapseOnSelect expand="lg" sticky="top" className="header">
+<Navbar collapseOnSelect expand="lg" sticky="top" className="header">
         <Nav.Link as={NavLink} to="./" className="header_navlink"> 
           <Navbar.Brand className="header_home">
              <HomeRounded />
           </Navbar.Brand>
         </Nav.Link>
 
-      <Navbar.Toggle />
-      
-      <Navbar.Collapse>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+      <Navbar.Collapse id="responsive-navbar-nav">
          <Nav className="mr-auto">
             {/*Resume link*/}
-            <Nav.Link as={NavLink} to="./" className={pathname == "/" ? "header_link_active" : "header_link"}>
+            <Nav.Link as={NavLink} href="#resume" to="./" className={pathname == "/" ? "header_link_active" : "header_link"}>
              Resume
             </Nav.Link>
 
             {/*Resume link*/}
-            <Nav.Link as={NavLink} to="/portfolio" className={pathname == "/portfolio" ? "header_link_active" : "header_link"}>
+            <Nav.Link as={NavLink} href="#portofolio" to="/portfolio" className={pathname == "/portfolio" ? "header_link_active" : "header_link"}>
              Portfolio
             </Nav.Link>
           </Nav>
+        <Nav>
          <div className="header_right">
             {Object.keys(ResumeData.socials).map((key) => (
-              <a href={ResumeData.socials[key].url} target="_blank">
+              <Nav.Link href={ResumeData.socials[key].url} target="_blank">
                 {ResumeData.socials[key].icon}
-              </a>
+              </Nav.Link>
+
             ))}
-           <a href={`mailto:${ResumeData.Email.url}`} className="Hire_me" target="_blank">
-            <CustomButton text={"Hire Me"} icon={<Telegram />}  />
-           </a>   
+           <Nav.Link href={`mailto:${ResumeData.Email.url}`} className="Hire_me" target="_blank">
+              <CustomButton text={"Hire Me"} icon={<Telegram />}  />
+           </Nav.Link>
           </div>
-         
-        
-        </Navbar.Collapse>
-     </Navbar>
-
-<>
-  <Navbar collapseOnSelect expand="lg"   className="header">
-  <Navbar.Brand href="#home" className="header_home">
-      <HomeRounded />
-  </Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
+        </Nav>
+      </Navbar.Collapse>
 </Navbar>
+ 
 </>
-
-
-
-   </>
     );
 }
 export default withRouter(Header);
