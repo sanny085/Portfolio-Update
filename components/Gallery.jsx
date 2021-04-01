a9b6ea5883689a9ac7ca3d6a26b4df96f6eb2345
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-01 16:05:14 +0530

    Add files via upload

---
+++ components/Gallery.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+
+import './Gallery.css';
+const Gallery = (props) => {
+    const imageList = props?.images?.map( (image) => {
+        return {
+            original: image,
+            thumbnail: image,
+        };
+     });
+    return (
+      <div>
+         <ImageGallery items={imageList} title={imageList} showBullets showThumbnails={false}/> 
+      </div>
+    );
+};
+
+export default Gallery;
