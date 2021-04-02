b7546e8c9b351279c03f5dc2fbf3339e605fd034
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-02 16:18:52 +0530

    Update Resume.jsx

--- pages/Resume/Resume.jsx
+++ pages/Resume/Resume.jsx
@@ -5,13 +5,13 @@ import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
 import TimelineConnector from "@material-ui/lab/TimelineConnector";
 import TimelineContent from "@material-ui/lab/TimelineContent";
 import TimelineDot from "@material-ui/lab/TimelineDot";
-import { Paper, Typography, Icon, TextField, Button } from "@material-ui/core";
-import { SchoolRounded, WorkRounded, Facebook, Twitter, LinkedIn, GitHub  } from "@material-ui/icons";
+import { Paper, Typography, Icon} from "@material-ui/core";
+import { SchoolRounded, WorkRounded} from "@material-ui/icons";
 import ResumeData from "../../utils/resumeDate";
 
 import "./Resume.css";
 import { Grid } from "@material-ui/core";
-import CustomButton from "../../components/Button/Button";
+ 
 
 const Resume = () => {
   return (
@@ -56,8 +56,8 @@ const Resume = () => {
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
-                {ResumeData.experiences.map((exp) => (
-                  <TimelineItem>
+              { ResumeData.experiences.map((exp, i) => (
+                  <TimelineItem key={i}>
                     <TimelineSeparator className="separator_padding">
                       <TimelineDot
                         variant="outlined"
@@ -80,7 +80,7 @@ const Resume = () => {
                       </Typography>
                     </TimelineContent>
                   </TimelineItem>
-                ))}
+                )) }
               </Timeline>
             </Grid>
             {/*Experiences*/}
@@ -99,8 +99,8 @@ const Resume = () => {
                     </Typography>
                   </TimelineContent>
                 </TimelineItem>
-                {ResumeData.education.map((edu) => (
-                  <TimelineItem>
+                {ResumeData.education.map((edu,j) => (
+                  <TimelineItem key={j}>
                     <TimelineSeparator className="separator_padding">
                       <TimelineDot
                         variant="outlined"
@@ -136,8 +136,8 @@ const Resume = () => {
             <h6 className="section_title_text">My Services</h6>
           </Grid>
           <Grid container spacing={3} justify="space-around">
-            {ResumeData.services.map((val) => { return(
-              <Grid item xs={12} sm={6} md={3}>
+            {ResumeData.services.map((val,k) => { return(
+              <Grid item xs={12} sm={6} md={3} key={k}>
                 <Paper elevation={0} className="service">
                   <div className="service_upper"><Icon className="service_icon">{val.icon}</Icon></div>
                   <Typography variant="h6" className="service_title">
@@ -160,14 +160,14 @@ const Resume = () => {
             <h6 className="section_title_text">My Skills</h6>
           </Grid>
           <Grid container spacing={3} justify={"space-between"}>
-            {ResumeData.skills.map((val) => { return (
-              <Grid item xs={12} sm={6} md={3}>
+            {ResumeData.skills.map((val1,l) => { return (
+              <Grid item xs={12} sm={6} md={3} key={l}>
                 <Paper elevation={0} className="skills">
                   <Typography variant="h6" className="skills_title">
-                    {val.title}
+                    {val1.title}
                   </Typography>
-                  {val.description.map((el) => (
-                    <Typography variant="body2" className="skills_description">
+                  {val1.description.map((el, x) => (
+                    <Typography variant="body2" className="skills_description" key={x}>
                       <TimelineDot
                         variant="outlined"
                         className="timeline_dot"/>
@@ -179,70 +179,9 @@ const Resume = () => {
             )} ) }
           </Grid>
         </Grid>
+      {/*End Skills*/}
+         
 
-        {/*Contact*/}
-        <Grid container className="section p_50 pb_50" spacing="10">
-          <Grid item xs={12} lg={7}>
-            <Grid container>
-              <Grid item className="section_title top_30">
-                <span></span>
-                <h6 className="section_title_text">Contact Form</h6>
-              </Grid>
-
-              <Grid item className="top_30">
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField fullWidth name="name" label="Name" />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField fullWidth name="email" label="E-mail" />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField fullWidth label="Message" multiline rows={4} />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <CustomButton text={"Submit"}/>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid item xs={12} lg={5}>
-            <Grid container>
-              <Grid item className="section_title top_30">
-                <span></span>
-                <h6 className="section_title_text">Contact Information</h6>
-              </Grid>
-              <Grid item xs={12} className="top_30">
-                <Grid container>
-                  <Grid item>
-                    <Typography className="contactInfo_item">
-                      <span>Address:</span> {ResumeData.address}
-                    </Typography>
-                    <Typography className="contactInfo_item">
-                      <span>Phone:</span> 
-                       <a className="mobResume" href={`tel: ${ResumeData.phone}`}>{" "}{ResumeData.phone}</a>
-                    </Typography>
-                        <a href={`mailto: ${ResumeData.Email.url}`} className="emailResume" >
-                        <Typography className="contactInfo_item">
-                            <span>Email:</span> {ResumeData.Email.subtitle}
-                        </Typography>
-                    </a>
-                    <Grid container className="contactInfo_socialsContainer">
-
-                      {Object.keys(ResumeData.socials).map(key => (
-                          <Grid item className="contactInfo_social">
-                            <a href={ResumeData.socials[key].url} target="_blank">{ResumeData.socials[key].icon}</a>
-                          </Grid>
-                      ))}
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
       </Grid>
     </Grid>
   );
