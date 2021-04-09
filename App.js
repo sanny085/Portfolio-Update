d5a7f8a60086e1c37352c51ebaa88c7f01b18e3d
SANNY KUMAR <49550883+sanny085@users.noreply.github.com>
2021-04-09 10:05:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Portfolio from './pages/Portfolio/Portfolio';
 import Resume from './pages/Resume/Resume';
+import Contact from './pages/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -15,20 +16,23 @@ function App() {
     <>
     <div className="App">
       <Container className="top_60">
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={12} md={4} lg={3}>
+        <Grid container spacing={2}>
+         <Grid item xs={12} sm={12} md={4} lg={3}>
              <Profile/>
-          </Grid> 
+         </Grid> 
          <Grid item xs={12} sm={12} md={8} lg={9} >
             <Router> 
               <Header/>
-                <div className="main_content container_shadow">
+                 <div className="main_content container_shadow">
                     <Switch>
                         <Route path="/Portfolio" component={Portfolio}>
                               <Portfolio/>
                         </Route>
+                        <Route path="/Contact" component={Contact}>
+                              <Contact/>
+                        </Route>
                         <Route path="/">
-                                <Resume/>
+                              <Resume/>
                         </Route>
                     </Switch>
                   </div>
